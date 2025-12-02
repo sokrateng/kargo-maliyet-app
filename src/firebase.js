7c57af60a180fe18224123247afe249aa30d4768
Engin Çoban <engin.coban@penta.com.tr>
2025-12-02 23:26:19 +0300

    Kargo uygulamasını güncelledim-5

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase konsolundan aldığınız bilgileri buraya yapıştırın
 const firebaseConfig = {
   apiKey: "AIzaSyAroQr11QOlWsU1s6get53kHBompbb9YFA",
   authDomain: "lojistiktasimamaliyet.firebaseapp.com",
@@ -16,6 +13,9 @@ const firebaseConfig = {
   measurementId: "G-0P4Y0LYVSR"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Aşağıdaki 'export' kelimeleri çok önemlidir. 
+// Bunlar olmadan App.jsx bu değişkenlere ulaşamaz.
+export const auth = getAuth(app);
+export const db = getFirestore(app);
